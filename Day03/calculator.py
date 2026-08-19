dd78ae7c6bfaed1144d227e98d0b006ac21639ab
sravanareddy91 <sravanareddy91@gmail.com>
2026-08-19 01:01:03 +0100

    using functions for calculator program

---
+++ Day03/calculator.py
@@ -0,0 +1,18 @@
+num1 = 10
+num2 = 5
+
+def addition():
+    addition = (num1 + num2)
+    print(addition)
+
+def substraction():
+    substraction = (num1 + num2)
+    print(substraction)
+
+def multiplication():
+    multiplication = (num1 + num2)
+    print(multiplication)
+
+addition()
+substraction()
+multiplication()
