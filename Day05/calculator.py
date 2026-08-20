dbe39681f9471b8cf5621fe25b5e71119469ad8f
sravanareddy91 <sravanareddy91@gmail.com>
2026-08-20 16:12:48 +0100

    different way of writing calculator program using return value

---
+++ Day05/calculator.py
@@ -0,0 +1,17 @@
+def add(num1, num2):
+    add = num1 + num2
+    return add
+
+def sub(num1, num2):
+    sub = num1 - num2
+    return sub
+
+def mul(num1, num2):
+    mul = num1 * num2
+    return mul
+
+print (add (5, 10))
+
+print (sub (10, 5))
+
+print (mul (10, 5))
