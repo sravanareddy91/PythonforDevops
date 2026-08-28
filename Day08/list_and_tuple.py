cc600d630a5c307a6e9abd9622bff30b98e3e001
sravanareddy91 <sravanareddy91@gmail.com>
2026-08-28 02:44:47 +0100

    adding or removing elements to list and checking if it works with tuple

---
+++ Day08/list_and_tuple.py
@@ -0,0 +1,5 @@
+
+
+s3_buckets_lists = ["sravana_demo_bucket", "ravi_demo_bucket", "atharva_demo_bucket", "cherry_demo_bucket", "teja_demo_bucket", "raviteja_demo_bucket"]
+s3_buckets_lists.append("new_s3_bucket")
+print(s3_buckets_lists)
