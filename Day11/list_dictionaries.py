438dd2dfebaa77588db3b473979f82fd6ca17626
sravanareddy91 <sravanareddy91@gmail.com>
2026-08-31 01:08:54 +0100

    program with list of dictionaries and printing the required output

---
+++ Day11/list_dictionaries.py
@@ -0,0 +1,15 @@
+ec2_instances_info = [
+    {
+        "id": "instance_001",
+        "type": "t2.micro"
+    },
+    {
+        "id": "instance_002",
+        "type": "t2.medium"
+    },
+    {
+        "id": "instance_003",
+        "type": "t2.xlarge"
+    }
+]
+print(ec2_instances_info[1]["type"])
